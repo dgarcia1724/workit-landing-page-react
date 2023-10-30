17d72d1cd9cbb967a6fd72b02255a29f3ca5614b
Danny Garcia <dgarcia1724@berkeley.edu>
2023-10-29 18:17:35 -0700

    hero done for tablet

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
 
 function Header() {
   return (
-    <header className="pt-[32px] px-[16px] bg-purple flex items-center justify-between">
+    <header className="pt-[32px] px-[16px] bg-purple flex items-center justify-between sm:px-[40px]">
       <img src="public/logo-light.svg" alt="logo" />
       <SecondaryBtn>Apply for access</SecondaryBtn>
     </header>
@@ -41,8 +41,8 @@ function Header() {
 
 function Hero() {
   return (
-    <div className="pt-[64px] px-[16px] pb-[156px] bg-purple text-center relative">
-      <h1 className="mb-[40px] fraunces font-semibold text-[50px] leading-[50px] tracking-0 text-white">
+    <div className="pt-[64px] px-[16px] pb-[156px] bg-purple text-center relative sm:pb-[203px]">
+      <h1 className="mb-[40px] fraunces font-semibold text-[50px] leading-[50px] tracking-0 text-white sm:text-[60px] leading-[60px]">
         Data <span className="secondaryBtnBorder">tailored</span> to your needs.
       </h1>
       <PrimaryBtn>Learn more</PrimaryBtn>
@@ -101,3 +101,6 @@ function Footer() {
     </footer>
   );
 }
+
+// 1. TODO Responsive for tablet next
+// 2. TODO Hero done, grid next for tablet
